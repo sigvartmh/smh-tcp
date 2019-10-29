ec43af1b0f9708ce81eee50b774b4fa9ab08358d
sigvartmh <sigvart.m@gmail.com>
2019-10-29 08:14:45 +0100

    script update

--- config.sh
+++ config.sh
@@ -1,1 +1,9 @@
+#!/bin/sh
+cargo b --release
+target=$CARGO_TARGET_DIR/release/smh-tcp
 sudo setcap cap_net_admin=eip $CARGO_TARGET_DIR
+sudo setcap cap_net_admin=eip $target
+$target & pid=$!
+sudo ip addr add 192.168.0.1/24 dev tun0
+sudo ip link set up dev tun0
+wait $pid
